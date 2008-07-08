6d49262bf280db68d3db4fed4063882cf5b3b4a0
matteo.caprari <matteo.caprari@9e7aba36-d54e-0410-a852-e9b781526091>
2008-07-08 14:27:05 +0000

    - fixed lineSelector - created square

--- blinkm-simulator/src/matteo/blinkm/console/LineSelector.java
+++ blinkm-simulator/src/matteo/blinkm/console/LineSelector.java
@@ -5,15 +5,17 @@ public class LineSelector {
 	public static char[] select(char[][] matrix, int i, int j, Direction dir, int len) {
 		char[] line = new char[len];
 		for (int xi=0; xi<len; xi++) {
-			System.out.print(xi +":" + xi*dir.y + ":" + j+xi*dir.x + ":" +(int)matrix[i+xi*dir.y][j+xi*dir.x] + " -- ");
-			line[xi] = matrix[i+xi*dir.y][j+xi*dir.x];
+			int x = i+xi*dir.y;
+			int y = j+xi*dir.x;
+			System.out.print(x+","+y+" ");
+			line[xi] = matrix[x][y];
 		}
 		System.out.println();
 		return line;
 	}
 	
 	public enum Direction {
-		north(-1,0), east(0,+1), south(+1,0), west(0,-1);
+		north(0,-1), east(+1,0), south(0,+1), west(-1,0);
 	
 		final int x;
 		final int y;
