b44699b2b486e849682da626de9178e77009da20
matteo.caprari <matteo.caprari@9e7aba36-d54e-0410-a852-e9b781526091>
2008-07-09 16:43:40 +0000

    - client made stateful - solved compilation errors

--- trunk/blinkm-simulator/src/matteo/blinkm/console/ProcessingSimulatorClient.java
+++ trunk/blinkm-simulator/src/matteo/blinkm/console/ProcessingSimulatorClient.java
@@ -5,6 +5,7 @@ import static matteo.blinkm.Definition.fadeToRGB;
 import java.awt.Color;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 import matteo.blinkm.graphics.Matrix;
@@ -12,6 +13,22 @@ import processing.net.*;
 
 public class ProcessingSimulatorClient {
 
+	private final String host;
+	private final int port;
+	private Socket socket;
+
+	public ProcessingSimulatorClient(String host, int port) {
+		this.host = host;
+		this.port = port;
+	}
+	
+	public void connect() {
+		try {
+			socket = new Socket(host, port);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 	public static void main(String[] args) {
 		int cols = 10;
 		int rows = 10;
@@ -32,33 +49,38 @@ public class ProcessingSimulatorClient {
 		.line(200, fadeToRGB, Color.blue)
 		.play((char)0, Matrix.square(matrix, 2, 2, 5)));
 		
-		Socket socket;
-		try {
-			socket = new Socket("localhost", 5204);
-			write(rt0, socket);
-			write(rt, socket);
-			//Thread.sleep(10000);
-			write(rt2, socket);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+		ProcessingSimulatorClient client = new ProcessingSimulatorClient("localhost", 5204);
+		client.connect();
+		client.write(new SimpleScript()
+		.line(25, fadeToRGB, Color.white)
+		.line(25, fadeToRGB, Color.gray)
+		.play((char)0, (char)0));
 	}
 
-	private static char[] getCharArray(
-			ArrayList<Character> installRedBlueOnASquare) {
-		char[] rt = new char[installRedBlueOnASquare.size()];
+	public static char[] getCharArray(ArrayList<Character> data) {
+		char[] rt = new char[data.size()];
 		for(int i=0; i<rt.length; i++) {
-			rt[i] = installRedBlueOnASquare.get(i);
+			rt[i] = data.get(i);
 		}
 		return rt;
 	}
+	
+	public void write(ArrayList<Character> data) {
+		write(getCharArray(data));
+	}
 
-	private static void write(char[] rt, Socket socket) throws IOException {
+	public void write(char[] rt) {
+		if (socket == null) 
+			return;
 		byte[] buf = new byte[rt.length];
 		for(int i=0; i<buf.length;i++)
 			buf[i] = (byte)rt[i];
 		System.out.println("writing buf " + buf.length);
-		socket.getOutputStream().write(buf);
-		socket.getOutputStream().flush();
+		try {
+			socket.getOutputStream().write(buf);
+			socket.getOutputStream().flush();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
