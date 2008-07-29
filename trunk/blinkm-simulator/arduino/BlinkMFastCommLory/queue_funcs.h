addfc83ffa7eb6d6b2a4c30e4f7802d2fed7df21
lagrespan <lagrespan@9e7aba36-d54e-0410-a852-e9b781526091>
2008-07-29 00:27:28 +0000

    tried a proper malloc() in queue_funcs.h; fixed the damn indentation

--- trunk/blinkm-simulator/arduino/BlinkMFastCommLory/queue_funcs.h
+++ trunk/blinkm-simulator/arduino/BlinkMFastCommLory/queue_funcs.h
@@ -2,9 +2,9 @@
 #define QUEUE_FUNCS_H
 
 struct element_t {
-        byte addr;
+	byte addr;
 	byte* payload;
-        byte len;
+	byte len;
 	struct element_t* next;
 };
 
@@ -15,28 +15,29 @@ struct queue_t {
 
 
 static int push(struct queue_t *q, int addr, byte* payload, int len) {
-  
-        // malloc 1 lorenzo 0
-  	struct element_t* el = (struct element_t*) malloc(sizeof(element_t));
-        
-        // check malloc
-        if ( el == NULL )
-          return -1;
-          
-        // init next pointer  
-        el->next = NULL;
-       
+
+	// malloc 1 lorenzo 0
+	//struct element_t* el = (struct element_t*) malloc(sizeof(element_t));
+	struct element_t* el = malloc(sizeof *el);
+
+	// check malloc
+	if ( el == NULL )
+		return -1;
+
+	// init next pointer  
+	el->next = NULL;
+
 	if ( q->head == NULL )
 		q->head = el;	// first element
 	else
 		q->tail->next = el;  // attach el to queue
-        
-        el->addr = addr;
+
+	el->addr = addr;
 	el->payload = payload;
-        el->len = len;
-        
+	el->len = len;
+
 	q->tail = el;  // end of queue points to this el
-        return 0;
+	return 0;
 }
 
 /* does not free() */
