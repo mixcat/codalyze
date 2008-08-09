d0c74bb739f1791fb041e9ff95f7f3390714a6af
lagrespan <lagrespan@9e7aba36-d54e-0410-a852-e9b781526091>
2008-08-09 16:56:09 +0000

    implemented adaptive jumping: when stuck in a local maximum (given by the ratio of change mutations/iterations) it will jump back to ground zero. ratio threshold adjusts itself according to the previous best result.

--- trunk/evo_sudoku/evo.c
+++ trunk/evo_sudoku/evo.c
@@ -7,21 +7,23 @@
  * row/column/sub-matrix
  *
  * lorenzo.grespan@gmail.com
- *
- *
- * TODO
- * genetic algo needs rethinking. 
- * if code gets stuck into a loop, perhaps it should
- * 'go back' and unroll the last mutation and try a different one
- * i.e. if fitness doesn't improve after trying all mutations
- *
- * idea: either save the mutations somewhere and randomly roll-back 
  */
 
 /* isdigit() */
 #include <ctype.h>	
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef CONSOLE
+#include <unistd.h>
+#endif
+
+#ifdef DEBUG
+#ifdef CONSOLE
+#define DBG(x);	x
+#else
+#define DBG(x);
+#endif
+#endif
 
 #define DATA_SIZE 	81
 #define MAX_FIT		81
@@ -36,7 +38,8 @@ usage(void) {
 }
 #endif
 
-static inline void print(int *gene_r[9][9], int *gene_c[9][9], int *gene_s[9][9]) {
+#ifdef CONSOLE
+static inline void print_genes(int *gene_r[9][9], int *gene_c[9][9], int *gene_s[9][9]) {
 	int i, j;
 	printf("gene\trow genes\t\tcolumn genes\t\tsub-square genes\n");
 	for ( i = 0 ; i < 9 ; i++ ) {
@@ -53,6 +56,7 @@ static inline void print(int *gene_r[9][9], int *gene_c[9][9], int *gene_s[9][9]
 		printf("\n");
 	}
 }
+#endif
 
 /*
  * old[0] is the position
@@ -78,7 +82,9 @@ mutate(int data[DATA_SIZE], int old[2], int mutation[2]) {
 	old[1] = data[pos];
 	/* save the mutation to be sent to the blinkms*/
 	mutation[0] = pos;
-	mutation[1] = random()%9;
+	do {
+		mutation[1] = random()%9;
+	} while ( old[1] == mutation[1] );
 	/* update the individual */
 	data[pos] = mutation[1];
 }
@@ -91,14 +97,14 @@ eval_fitness(int *gene_r[9][9], int *gene_c[9][9], int *gene_s[9][9]) {
 	int digits = 0;
 	int values_row[9], values_col[9], values_sub[9];
 
-	/* zero the arrays before starting */
 	for ( i = 0 ; i < 9 ; i++ ) {
-		values_row[i] = 0;
-		values_col[i] = 0;
-		values_sub[i] = 0;
-	}
+		/* zero the arrays before starting */
+		for ( j = 0 ; j < 9 ; j++ ) {
+			values_row[j] = 0;
+			values_col[j] = 0;
+			values_sub[j] = 0;
+		}
 
-	for ( i = 0 ; i < 9 ; i++ ) {
 		for ( j = 0 ; j < 9 ; j++ ) {
 			/* count the number of digits */
 			values_row[*gene_r[i][j]]++;
@@ -133,6 +139,8 @@ eval_fitness(int *gene_r[9][9], int *gene_c[9][9], int *gene_s[9][9]) {
 	return fit;
 }
 
+/*********/
+
 #ifdef CONSOLE
 int main ( int argc, char **argv ) {
 #else
@@ -140,10 +148,13 @@ int main () {
 #endif
 	/* C90 forbids mixed declaration and code */
 	int i;
-	int data[DATA_SIZE];
 	int *gene_r[9][9], *gene_c[9][9], *gene_s[9][9];
 	int fit, new_fit;
 	int iter = 0;
+	int mutations;	/* the number of successful mutations */
+	int orig_fit;
+	float jumps_threshold = 0.01;
+	int peak, old_peak;
 
 	/*
 	 * old[0] is the position
@@ -152,40 +163,63 @@ int main () {
 	int old[2], mutation[2];	
 
 #ifdef CONSOLE
-	char c[2];
-	FILE *in;
+	/* if CONSOLE is defined -> code for console, read from file */
+	int data[DATA_SIZE];
 
-	if ( argc != 3 ) {
-		usage();
-		return -1;
-	}
+	{
+		char c[2];
+		FILE *in;
 
-	in = fopen(argv[1], "r");
-	if ( in == NULL ) {
-		perror("Unable to open file");
-		return -1;
-	}
+		if ( argc != 3 ) {
+			usage();
+			return -1;
+		}
 
-	/* atoi() reads char* until \0;
-	 * if the next thing in memory after 'c' were a number
-	 * I'd be screwed :)
-	 * */
-	c[1] = '\0';	
-	i = 0 ;
+		in = fopen(argv[1], "r");
+		if ( in == NULL ) {
+			perror("Unable to open file");
+			return -1;
+		}
 
-	while ( (c[0] = fgetc(in)) != EOF && i <= DATA_SIZE ) 
-		if ( isdigit(c[0]) )
-			data[i++] = atoi(c);
-		else
-			if ( isspace(c[0]) )
-				/* for newline */
-				continue;	
+		/* atoi() reads char* until \0;
+		 * if the next thing in memory after 'c' were a number
+		 * I'd be screwed :)
+		 * */
+		c[1] = '\0';	
+		i = 0 ;
+
+		while ( (c[0] = fgetc(in)) != EOF && i <= DATA_SIZE ) 
+			if ( isdigit(c[0]) )
+				data[i++] = atoi(c);
 			else
-				/* everything else */
-				data[i++] = 0;	
+				if ( isspace(c[0]) )
+					/* for newline */
+					continue;	
+				else
+					/* everything else */
+					data[i++] = 0;	
 
-	/* init random number generator */
-	srandom(atoi(argv[2]));
+		/* init random number generator */
+		srandom(atoi(argv[2]));
+	}
+
+#else
+
+	/* if no console -> is code for arduino */
+
+	/* the sudoku */
+	int data[] = { 
+			8, 4, 0, 0, 0, 0, 0, 5, 7,
+			0, 7, 2, 0, 0, 0, 9, 8, 0,
+			0, 0, 9, 0, 0, 0, 2, 0, 0,
+			0, 0, 0, 4, 0, 6, 0, 0, 0,
+			1, 0, 0, 5, 0, 3, 0, 0, 8,
+			0, 0, 0, 7, 0, 9, 0, 0, 0,
+			0, 0, 8, 0, 0, 0, 6, 0, 0,
+			0, 9, 5, 0, 0, 0, 7, 1, 0,
+			7, 3, 0, 0, 0, 0, 0, 4, 5
+	};
+
 #endif
 
 	/* generate the genotypes */
@@ -225,14 +259,16 @@ int main () {
 			original[i] = data[i];
 		}
 	}
-	print(gene_r, gene_c, gene_s);
 
 	fit = eval_fitness(gene_r, gene_c, gene_s);
+	orig_fit = fit;
+	mutations = 0;
 
 	while ( fit <= MAX_FIT ) {
 		iter++;
-		printf(".");
+		/* DBG is already defined only if CONSOLE is defined */
 		mutate(data, old, mutation);
+
 		/* TODO 
 		 * insert code here to send data to blinkm
 		 * to show mutations 
@@ -241,15 +277,58 @@ int main () {
 		 */
 		new_fit = eval_fitness(gene_r, gene_c, gene_s);
 
-		if ( fit > new_fit )  
+		if ( fit >= new_fit ) {
 			/* go back to before the mutation */
 			reverse_mutation(data, old);
+
+			if ( mutations > 0 && (float)mutations/iter < jumps_threshold ) {
+				DBG(
+					printf("old fitness: %d, new fit: %d\n", fit, new_fit);
+					printf("mutations: %d, iterations: %d, ratio: %f\n", mutations, iter, (float)mutations/iter);
+					printf("rollback! old genes:\n");
+				   );
+				print_genes(gene_r, gene_c, gene_s);
+				/* we're in a local maximum; jump away */
+				for ( i = 0 ; i < DATA_SIZE ; i++ )
+					data[i] = original[i];
+				/* save the old best value */
+				old_peak = peak;
+				peak = fit;
+				/* reset everything */
+				fit = orig_fit;
+				mutations = 0;
+				iter = 0;
+				/* well */
+				DBG(sleep(10););
+
+				if ( old_peak > peak )
+					jumps_threshold /= 2;
+				else
+					if ( old_peak < peak ) 
+						jumps_threshold *= 2;
+						
+				printf("new jump threshold: %f\n\n", jumps_threshold);
+			}
+		}
 		else {
-			printf("\n(%d): got a valid offspring; old fitness: %d, new fit: %d\n", iter, fit, new_fit);
-			print(gene_r, gene_c, gene_s);
+			/* DBG is already defined only if CONSOLE is defined */
+			/*
+			DBG(
+				printf("mutation pos: %d %d->%d", old[0], old[1], mutation[1]);
+				printf("\n(%d): got a valid offspring; ", iter);
+				printf("old fitness: %d, new fit: %d\n", fit, new_fit);
+				print_genes(gene_r, gene_c, gene_s);
+			);
+			*/
 			fit = new_fit;
+			mutations++;
 		}
 	}
+
+#ifdef CONSOLE
+	printf("Found solution after %d iterations:\n", iter);
+	print_genes(gene_r, gene_c, gene_s);
+#endif
 
 	return 0;
 		
