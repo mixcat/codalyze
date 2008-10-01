c86ea43ade8fc4b33d8f90f17151bce05afa3ae5
matteo.caprari <matteo.caprari@9e7aba36-d54e-0410-a852-e9b781526091>
2008-10-01 16:23:31 +0000

    - yui css in skin @import

--- dk.flickaway/trunk/src/main/webapp/faw-loader.js
+++ dk.flickaway/trunk/src/main/webapp/faw-loader.js
@@ -1,1 +1,4 @@
-$(function() { $.ajax({url:'faw-core-1.0.0.js', dataType:'script', cache:true}); });
+$('<link rel="stylesheet" type="text/css" media="screen" href="faw-skin-default.css"/>').appendTo('head');
+$(function() {
+	$.ajax({url:'faw-core-1.0.0.js', dataType:'script', cache:true});
+});
